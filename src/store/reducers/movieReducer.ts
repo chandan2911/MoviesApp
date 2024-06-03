54c8825aafc356e6aeee28e1bb3fc80c29e86f3f
Chandan <chandan.chaurasia@quizizz.com>
2024-06-03 08:36:36 +0530

    feat: accordian done

--- src/store/reducers/movieReducer.ts
+++ src/store/reducers/movieReducer.ts
@@ -12,7 +12,7 @@ export interface Movie {
   adult: boolean;
   backdrop_path: string;
   genre_ids: number[];
-  id: number;
+  id: string;
   original_language: string;
   original_title: string;
   overview: string;
